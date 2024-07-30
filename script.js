3c74853fa80e14e359ea231df968c084b9bada4e
Austin <austinematurure@gmail.com>
2024-07-30 19:47:38 +0300

    Added Username Message on Set

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-container = document.getElementById('#container')
+const container = document.getElementById('container');
+const btnUsername = document.getElementById('btnUsername');
+const inputUsername = document.getElementById('inputUsername');
+
+btnUsername.onclick = function() {
+    container.innerHTML = `<h1> hi ${inputUsername.value}</h1>`;
+};
